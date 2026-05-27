62f0f61b54fe3fa85455b40e237aa3222367041f
mustafa-hamdan05 <mustafahamdan765@gmail.com>
2026-05-27 08:36:29 +0200

    Add backend homepage

---
+++ backend/src/main/java/com/gigflow/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.gigflow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "QuickHire Backend API is running successfully!";
+    }
+}
